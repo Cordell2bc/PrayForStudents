e4e16414f7d0577255220b124ffeb1faeaefdc95
jordanbakersays <jordanbakersays@gmail.com>
2026-08-24 22:43:42 -0400

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("push", event => {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || "Calvary Students", {
+      body: data.body || "Time to pray for your students 🙏",
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      tag: "prayer-reminder",
+      renotify: true,
+    })
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("/"));
+});
