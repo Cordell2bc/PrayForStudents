3d726cde6ae524b7928173b8831157b4a4a36810
Cordell2bc <cordell@2bclafayette.com>
2026-09-23 11:07:47 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("push", event => {
   const data = event.data ? event.data.json() : {};
   event.waitUntil(
-    self.registration.showNotification(data.title || "Calvary Students", {
+    self.registration.showNotification(data.title || "2ND Students", {
       body: data.body || "Time to pray for your students 🙏",
       icon: "/icon-192.png",
       badge: "/icon-192.png",
